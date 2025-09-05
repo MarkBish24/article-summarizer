cb459a799b1c5d03fcc9a2912792728e285e6e09
Mark Bishoff <markbish24@gmail.com>
2025-09-05 05:13:28 -0700

    created a virutal environment so it works now

--- NLP/server.py
+++ NLP/server.py
@@ -1,13 +1,18 @@
 from flask import Flask, request, jsonify
 import article_summarizer as summarizer
 
+
 app = Flask(__name__)
 
-@app.route("/summarize", methods=["POST"])
+@app.route("/summarize", methods=["GET", "POST"])
 def summarize_route():
-    
-    data = request.get_json()
-    text = data.get("text", "")
+    data = request.get_json(silent=True)
+
+    if data and "text" in data:
+        text = data["text"]
+    else:
+        with open("text.txt", "r", encoding="utf-8") as f:
+            text = f.read()
 
     if not text:
         return jsonify({"error": "No text provided"}), 400
